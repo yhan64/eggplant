22e2034a87e15682dfc2b28102ac22f2aaee8c9f
yhan <shelly.y.han@gmail.com>
2018-05-18 22:11:12 -0700

    add eslint

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,14 @@
 
 import http from 'http';
-import fs from 'fs';
-import path from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
+
 const app = express();
 const port = process.env.NODE_ENV === 'production' ? 80 : 4321;
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('pageok'))
+app.get('/', (req, res) => res.send('pageok'));
 
 http.createServer(app).listen(port, () => {
-  console.log(`Started listening on port ${port}!`)
+  console.log(`Started listening on port ${port}!`);
 });
