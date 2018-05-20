8e1a125f5ffc991083759571cc91026937328e44
Fan Yu <yufan2081@gmail.com>
2018-05-20 15:56:12 -0700

    Reformat code.

--- frontend/src/components/Chart.js
+++ frontend/src/components/Chart.js
@@ -5,27 +5,27 @@ class Chart extends React.Component {
   componentDidMount() {
     Highcharts.chart('chartContainer', {
       chart: {
-          type: 'bar'
+        type: 'bar'
       },
       xAxis: {
-          categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
       },
-  
+
       plotOptions: {
-          series: {
-              cursor: 'pointer',
-              events: {
-                  click: (event) => {
-                      console.log(event.point.options);
-                  }
-              }
+        series: {
+          cursor: 'pointer',
+          events: {
+            click: (event) => {
+              console.log(event.point.options);
+            }
           }
+        }
       },
-  
+
       series: [{
-          data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
       }]
-  });
+    });
   }
   render() {
     return <div id="chartContainer" />;
