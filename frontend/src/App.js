ee8d71b9dcd404de8b4856469bbd5531f5fd34d7
yhan <shelly.y.han@gmail.com>
2018-05-20 15:51:20 -0700

    move Chart.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import Chart from './Chart';
+import Chart from './components/Chart';
 import './App.css';
 
 const App = () => {
