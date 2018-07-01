a6c88e11f17dc4a1c4e363aaa5b7174a2c73a3f1
Fan Yu <yufan2081@gmail.com>
2018-07-01 13:49:56 -0700

    Change app to class structure.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,54 +6,59 @@ import CreateTask from './components/CreateTask';
 import UpdateTask from './components/UpdateTask';
 import './App.css';
 
-const App = (props) => {
-  const ADD_STATE = 0;
-  const EDIT_STATE = 1;
+const ADD_STATE = 0;
+const EDIT_STATE = 1;
 
-  const toggleAdd = () => {
-    props.rightPanelState = ADD_STATE;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rightPanelState: ADD_STATE,
+    };
+  }
+
+  toggleAdd = () => {
+    this.setState({ rightPanelState: ADD_STATE });
   };
 
-  const toggleEdit = (val) => {
-    props.rightPanelState = EDIT_STATE;
-    console.log(props.rightPanelState, val);
+  toggleEdit = (val) => {
+    this.setState({ rightPanelState: EDIT_STATE });
+    console.log(this.state.rightPanelState, val);
   };
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Eggplant</h1>
-      </header>
-      <div className="App-container">
-        <div className="App-chart-area">
-          <Button
-            variant="fab"
-            mini
-            color="primary"
-            aria-label="add"
-            className="App-add-button"
-            onClick={toggleAdd}
-          >
-            <AddIcon />
-          </Button>
-          <Chart onSerieClick={toggleEdit} />
-        </div>
-        <div className="App-splitter" />
-        <div className="App-detail-area">
-          {props.rightPanelState === ADD_STATE &&
-            <CreateTask />
-          }
-          {props.rightPanelState === EDIT_STATE &&
-            <UpdateTask />
-          }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Eggplant</h1>
+        </header>
+        <div className="App-container">
+          <div className="App-chart-area">
+            <Button
+              variant="fab"
+              mini
+              color="primary"
+              aria-label="add"
+              className="App-add-button"
+              onClick={this.toggleAdd}
+            >
+              <AddIcon />
+            </Button>
+            <Chart onSerieClick={this.toggleEdit} />
+          </div>
+          <div className="App-splitter" />
+          <div className="App-detail-area">
+            {this.state.rightPanelState === ADD_STATE &&
+              <CreateTask />
+            }
+            {this.state.rightPanelState === EDIT_STATE &&
+              <UpdateTask />
+            }
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-App.defaultProps = {
-  rightPanelState: 0,
-};
+    );
+  }
+}
 
 export default App;
